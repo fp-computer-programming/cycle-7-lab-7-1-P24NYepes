e89e87b6311485ab8c42cb04e82546f6ef09bc25
Nicholas Yepes <p24nyepes@fairfieldprep.org>
2023-11-16 11:25:07 -0500

    submission
    hello :)

--- lab_7-2.py
+++ lab_7-2.py
@@ -9,3 +9,16 @@ Add a statement in the function to print "Hello World!"
 Add a statement that returns the docstring for the greeting function
 Add another statement to call the function
 """
+
+#Author: Nicholas Yepes 11/16/23
+
+#deffines greeting 
+def greeting():
+    
+    #prints out hello world!
+    print("Hello World!") 
+    # returns the docstring
+    return greeting.__doc__
+    
+#prints out the returend docstring
+print(greeting())
